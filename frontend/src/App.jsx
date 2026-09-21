94c5fc9f46310f9509169f00f0d70324e663445f
Debora Barolin <barolindebora224@gmail.com>
2026-09-21 19:18:40 -0300

    Back terminado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-lg">
+        <h1 className="text-4xl font-bold text-purple-600">
+          AMOR ANIMAL by Debi 🐾
+        </h1>
+
+        <p className="mt-3 text-gray-600">
+          Sistema de gestión Pet Shop
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
